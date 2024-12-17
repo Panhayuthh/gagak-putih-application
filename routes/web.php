525a0af6931c486cbda0993bcb94a175cd4f83fc
Panhayuth Oeun <panhayuth11@gmail.com>
2024-12-17 19:49:45 +0700

    fix: testing frontend

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('home');
+    return view('welcome');
 });
 
 Route::get('/home', function () {
