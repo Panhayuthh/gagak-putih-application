7f5bf575b2c176c74682c415c6c601901ca9552b
Panhayuth Oeun <panhayuth11@gmail.com>
2024-12-18 10:49:55 +0700

    fix: component from tailwind to bootstrap

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
@@ -26,4 +26,40 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout> --}}
+
+@extends('layouts.app')
+
+@section('title', 'Profile')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Profile') }}</div>
+
+                    <div class="card-body">
+                        @include('profile.partials.update-profile-information-form')
+                    </div>
+                </div>
+
+                <div class="card mt-4">
+                    <div class="card-header">{{ __('Password') }}</div>
+
+                    <div class="card-body">
+                        @include('profile.partials.update-password-form')
+                    </div>
+                </div>
+
+                <div class="card mt-4">
+                    <div class="card-header">{{ __('Delete Account') }}</div>
+
+                    <div class="card-body">
+                        @include('profile.partials.delete-user-form')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
